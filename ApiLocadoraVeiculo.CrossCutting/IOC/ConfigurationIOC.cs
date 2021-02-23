e74394f3b3af84857ed0f7931797ed6acfdb6166
douglas eduardo <douglaseduardo220483@gmail.com>
2021-02-23 15:42:38 -0300

    terminando a injeção de dependecias

---
+++ ApiLocadoraVeiculo.CrossCutting/IOC/ConfigurationIOC.cs
@@ -0,0 +1,31 @@
+﻿using ApiLocadoraVeiculo.Application;
+using ApiLocadoraVeiculo.Application.Interfaces;
+using ApiLocadoraVeiculo.Application.Interfaces.Mappers;
+using ApiLocadoraVeiculo.Application.Mappers;
+using ApiLocadoraVeiculo.Domain.Core.Interfaces.Repositorys;
+using ApiLocadoraVeiculo.Domain.Core.Interfaces.Services;
+using ApiLocadoraVeiculo.Domain.Services;
+using ApiLocadoraVeiculo.Infrastructure.Data.Repositorys;
+using Autofac;
+
+namespace ApiLocadoraVeiculo.CrossCutting.IOC
+{
+    public class ConfigurationIOC
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IOC
+
+            builder.RegisterType<ApplicationServiceCliente>().As<IApplicationServiceCliente>();
+            builder.RegisterType<ApplicationServiceVeiculo>().As<IApplicationServiceVeiculo>();
+            builder.RegisterType<ServiceCliente>().As<IServiceCliente>();
+            builder.RegisterType<ServiceVeiculo>().As<IServiceVeiculo>();
+            builder.RegisterType<RepositoryCliente>().As<IRepositoryCliente>();
+            builder.RegisterType<RepositoryVeiculo>().As<IRepositoryVeiculo>();
+            builder.RegisterType<MapperCliente>().As<IMapperCliente>();
+            builder.RegisterType<MapperVeiculo>().As<IMapperVeiculo>();
+
+            #endregion IOC
+        }
+    }
+}
---
+++ ApiLocadoraVeiculo.CrossCutting/IOC/ModuleIOC.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace ApiLocadoraVeiculo.CrossCutting.IOC
+{
+    public class ModuleIOC : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            ConfigurationIOC.Load(builder);
+        }
+    }
+}
+
+// manipulação de dados; performace do banco de dados; estudar sobre task (async, await);
