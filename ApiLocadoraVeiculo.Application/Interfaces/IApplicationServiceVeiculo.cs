2e14fbf968276c587465549b118a8cb2ffc39aca
douglas eduardo <douglaseduardo220483@gmail.com>
2021-02-23 11:33:09 -0300

    Criando um CrossCutting para tentar isolar essa camada ainda mais

---
+++ ApiLocadoraVeiculo.Application/Interfaces/IApplicationServiceVeiculo.cs
@@ -0,0 +1,18 @@
+﻿using ApiLocadoraVeiculo.Application.Dtos;
+using System.Collections.Generic;
+
+namespace ApiLocadoraVeiculo.Application.Interfaces
+{
+    public interface IApplicationServiceVeiculo
+    {
+        void Add(VeiculoDto veiculo);
+
+        void Update(VeiculoDto veiculo);
+
+        void Remove(VeiculoDto veiculo);
+
+        IEnumerable<VeiculoDto> GetAll();
+
+        VeiculoDto GetById(int id);
+    }
+}
